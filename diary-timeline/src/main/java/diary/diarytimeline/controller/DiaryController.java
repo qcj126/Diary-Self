90ad44d54c964669274e23581a556f13591a426c
qcj126 <1262901257@qq.com>
2026-04-09 22:59:04 +0800

    main

---
+++ diary-timeline/src/main/java/diary/diarytimeline/controller/DiaryController.java
@@ -0,0 +1,9 @@
+package diary.diarytimeline.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/timeline/diary")
+public class DiaryController {
+}
