8f3860e3fe395ebab83a8ba8192eb0090055ac3c
Chase <1262901257@qq.com>
2026-05-08 10:21:07 +0800

    分页查询

--- diary-recipe/src/main/java/diary/recipe/impl/query/RecipeQueryServiceImpl.java
+++ diary-recipe/src/main/java/diary/recipe/impl/query/RecipeQueryServiceImpl.java
@@ -1,84 +1,34 @@
 package diary.recipe.impl.query;
 
-import diary.common.entity.recipe.ao.AuthorInfoAO;
-import diary.common.entity.recipe.dto.RecipeReqDto;
-import diary.common.entity.recipe.po.RecipeIngredientPO;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+
+import diary.common.entity.recipe.dto.req.RecipePageReqDto;
+
+import diary.common.entity.recipe.dto.resp.PageRespDto;
+import diary.common.entity.recipe.dto.resp.RecipeRespDto;
+
 import diary.common.entity.recipe.po.RecipePO;
-import diary.common.entity.recipe.po.RecipeStepPO;
-import diary.common.entity.recipe.vo.IngredientVO;
-import diary.common.entity.recipe.vo.RecipeVO;
-import diary.common.entity.recipe.vo.StepVO;
-import diary.common.entity.user.po.User;
-import diary.common.result.ApiResponse;
-import diary.dao.mapper.recipe.RecipeIngredientMapper;
 import diary.dao.mapper.recipe.RecipeMapper;
-import diary.dao.mapper.recipe.RecipeStepMapper;
-import diary.dao.mapper.user.UserMapper;
 import jakarta.annotation.Resource;
-import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import diary.recipe.service.query.RecipeQueryService;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @Service
 public class RecipeQueryServiceImpl implements RecipeQueryService {
     @Resource
     private RecipeMapper recipeMapper;
 
-    @Resource
-    private RecipeIngredientMapper recipeIngredientMapper;
-
-    @Resource
-    private RecipeStepMapper recipeStepMapper;
-
-    @Resource
-    private UserMapper userMapper;
-
     @Override
-    public ApiResponse<Map<String, Object>> queryRecipe(RecipeReqDto recipeReqDto) {
-        if (recipeReqDto == null) {
-            throw new RuntimeException("参数不能为空");
+    public PageRespDto<RecipeRespDto> pageQueryRecipe(RecipePageReqDto recipePageReqDto) {
+        // 若参数全部为空，则默认查询最新的n条数据
+        IPage<RecipePO> page = new Page<>(recipePageReqDto.getPageNum(), recipePageReqDto.getPageSize());
+        IPage<RecipePO> pageResult = recipeMapper.qryPage(page, recipePageReqDto);
+        IPage<RecipeRespDto> pageResultDto = new Page<>();
+        for (RecipePO recipe : pageResult.getRecords()) {
+            RecipeRespDto recipeRespDto = RecipeRespDto.fromEntity(recipe);
+            pageResultDto.getRecords().add(recipeRespDto);
         }
-
-        if (recipeReqDto.getRecipeId() == null) {
-            throw new RuntimeException("食谱id不能为空");
-        }
-
-        RecipePO recipePO = recipeMapper.selectById(recipeReqDto.getRecipeId());
-        List<RecipeIngredientPO> recipeIngredientPOs = recipeIngredientMapper.selectByRecipeId(recipeReqDto.getRecipeId());
-        List<RecipeStepPO> recipeStepPOs = recipeStepMapper.selectByRecipeId(recipeReqDto.getRecipeId());
-        User author = userMapper.selectByUserId(recipePO.getAuthorId());
-        Long userId = author.getUserId();
-        String username = author.getUsername();
-        AuthorInfoAO authorInfoAO = new AuthorInfoAO();
-        authorInfoAO.setUserId(userId);
-        authorInfoAO.setNickname(username);
-
-        RecipeVO recipeVO = new RecipeVO();
-        List<IngredientVO> ingredientVOs = new ArrayList<>();
-        List<StepVO> stepVOs = new ArrayList<>();
-        for (RecipeIngredientPO recipeIngredientPO : recipeIngredientPOs) {
-            IngredientVO IngredientVO = new IngredientVO();
-            BeanUtils.copyProperties(recipeIngredientPO, IngredientVO);
-            ingredientVOs.add(IngredientVO);
-        }
-
-        for (RecipeStepPO recipeStepPO : recipeStepPOs) {
-            StepVO stepVO = new StepVO();
-            BeanUtils.copyProperties(recipeStepPO, stepVO);
-            stepVOs.add(stepVO);
-        }
-        BeanUtils.copyProperties(recipePO, recipeVO);
-        recipeVO.setIngredients(ingredientVOs);
-        recipeVO.setSteps(stepVOs);
-        recipeVO.setAuthor(authorInfoAO);
-        Map<String, Object> result = new HashMap<>();
-        result.put("recipe", recipeVO);
-
-        return ApiResponse.success(result);
+        return PageRespDto.of(pageResultDto);
     }
 }
