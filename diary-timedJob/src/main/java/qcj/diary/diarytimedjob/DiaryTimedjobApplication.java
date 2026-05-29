ddb7fcf4b686e51b596a159d63f4dfe200770dfe
Chase <1262901257@qq.com>
2026-05-29 15:06:20 +0800

    模块

---
+++ diary-timedJob/src/main/java/qcj/diary/diarytimedjob/DiaryTimedjobApplication.java
@@ -0,0 +1,13 @@
+package qcj.diary.diarytimedjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiaryTimedjobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiaryTimedjobApplication.class, args);
+    }
+
+}
