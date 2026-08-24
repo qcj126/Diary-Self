7b4692cbe991181908c94fdc12b0406161903c2b
qcj126 <1262901257@qq.com>
2026-08-25 00:19:33 +0800

    main

--- diary-leetcode/src/main/java/diary/diaryleetcode/两个数组的交集349.java
+++ diary-leetcode/src/main/java/diary/diaryleetcode/两个数组的交集349.java
@@ -2,14 +2,16 @@ package diary.diaryleetcode;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 
 public class 两个数组的交集349 {
     public static void main(String[] args) {
-        int[] nums1 = {1,4,3,4,5,2,1};
-        int[] nums2 = {4,5,6};
+        int[] nums2 = {1,4,3,4,5,2,1};
+        int[] nums1 = {4,5,6};
         int[] result = intersection1(nums1, nums2);
         int[] result2 = intersection2(nums1, nums2);
+        int[] result3 = intersection3(nums1, nums2);
     }
 
     public static int[] intersection1(int[] nums1, int[] nums2) {
@@ -68,15 +70,19 @@ public class 两个数组的交集349 {
         Arrays.sort(nums1);
         Arrays.sort(nums2);
 
-        for (int i = 0; i < nums1.length;) {
-            for (int j = 0; j < nums2.length;) {
-                if (nums1[i] == nums2[j]) {
-                    hashSet.add(nums1[i]);
-                    i++;
+        for (int i = 0, j = 0; i < nums1.length && j < nums2.length;) {
+            if (nums1[i] == nums2[j]) {
+                hashSet.add(nums1[i]);
+                i++;
+                j++;
+            } else {
+                if (nums1[i] > nums2[j]) {
                     j++;
                 }
-                if (nums1[i] > nums2[j]) j++;
-                if (nums1[i] < nums2[j]) i++;
+                if (j == nums2.length) break;
+                if (nums1[i] < nums2[j]) {
+                    i++;
+                }
             }
         }
         int[] ints = new int[hashSet.size()];
@@ -86,5 +92,85 @@ public class 两个数组的交集349 {
             i++;
         }
         return ints;
+    }
+
+    // 两个数组的交集 II
+    public int[] intersect4(int[] nums1, int[] nums2) {
+        // 10ms 43.89MB
+        ArrayList<Integer> hashSet1 = new ArrayList<>();
+        ArrayList<Integer> hashSet3 = new ArrayList<>();
+        for (int i = 0; i < nums1.length; i++) {
+            hashSet1.add(nums1[i]);
+        }
+
+        for (Integer i : nums2) {
+            if (hashSet1.contains(i)) {
+                hashSet3.add(i);
+                hashSet1.remove(i);
+            }
+        }
+        int[] ints = new int[hashSet3.size()];
+        int i = 0;
+        for (Integer integer : hashSet3) {
+            ints[i] = integer;
+            i++;
+        }
+        return ints;
+    }
+
+    // 两个数组的交集 II
+    public int[] intersect6(int[] nums1, int[] nums2) {
+        // 3ms 44.61MB
+        // 第一个是小集合
+        if (nums1.length > nums2.length) {
+            return intersect6(nums2, nums1);
+        }
+        HashMap<Integer, Integer> map = new HashMap<>();
+        HashMap<Integer, Integer> resultMap = new HashMap<>();
+        for (int i = 0; i < nums1.length; i++) {
+            map.put(nums1[i], map.getOrDefault(nums1[i], 0) + 1);
+        }
+        int index = 0;
+        for (int i = 0; i < nums2.length; i++) {
+            if (map.containsKey(nums2[i])) {
+                if (map.get(nums2[i]) > 0) {
+                    map.put(nums2[i], map.get(nums2[i]) - 1);
+                    resultMap.put(index ++, nums2[i]);
+                } else {
+                    map.remove(nums2[i]);
+                }
+            }
+        }
+        int[] ints = new int[resultMap.size()];
+        for (int x = 0; x < resultMap.size(); x++) {
+            ints[x] = resultMap.get(x);
+        }
+        return ints;
+    }
+
+    // 两个数组的交集 II
+    public int[] intersect5(int[] nums1, int[] nums2) {
+        // 3ms 44.43MB
+        // 第一个是小集合
+        if (nums1.length > nums2.length) {
+            return intersect5(nums2, nums1);
+        }
+        int[] ints = new int[nums1.length];
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums1.length; i++) {
+            map.put(nums1[i], map.getOrDefault(nums1[i], 0) + 1);
+        }
+        int index = 0;
+        for (int i = 0; i < nums2.length; i++) {
+            if (map.containsKey(nums2[i])) {
+                if (map.get(nums2[i]) > 0) {
+                    map.put(nums2[i], map.get(nums2[i]) - 1);
+                    ints[index ++] = nums2[i];
+                } else {
+                    map.remove(nums2[i]);
+                }
+            }
+        }
+        return Arrays.copyOfRange(ints, 0, index);
     }
 }
